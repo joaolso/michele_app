bc74598ff1ae1e74ae9972985b0bf361f54bcfc6
joaolso <lucas.xms@gmail.com>
2021-07-04 16:45:30 -0300

    fix menu navigation and bug fix

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
+import streamlit as st
+
+st.set_page_config(
+        page_title="App",
+        page_icon="📚",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
 import numpy as np
 import pandas as pd
-import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 #import modules
 from function import *
+from multiapp import MultiApp
+from pages import shap, lime, exploratory # import your app modules here
 
-# Globals
-DATA_PATH = 'data/dados.csv'
 
-st.title('Dados Discentes')
+PAGES = {
+    "SHAP": shap,
+    "LIME": lime,
+    "Exploratory": exploratory,
+}
 
-#Load Data
-data = load_data(DATA_PATH)
-st.markdown('### Raw Data')
-st.dataframe(data.head().astype('object'))
+def main():
 
-data = transform_data(data)
+    st.sidebar.markdown("## Menu")
+    selection = st.sidebar.radio("", list(PAGES.keys()))
 
-data_train = data_train_generate(data)
-st.markdown('### Transformed Data')
-st.dataframe(data_train.head().astype('object'))
+    page = PAGES[selection]
+    page.app()
 
-# Instantiating the SHAP explainer using our trained RFC model
-st.markdown('### SHAP in Streamlit')
-train_and_SHAP(data_train)
+    st.sidebar.markdown("## Release")
+    st.sidebar.info(
+        "Aplicação de Desenvolvimento para identificação de atributos relevantes"
+        " em dados estudantis"
+    )
+    st.sidebar.markdown("## Versão 1.0.0")
+    st.sidebar.info(
+        """ This app is maintained by """
+    )
+
+
+if __name__ == "__main__":
+    main()
